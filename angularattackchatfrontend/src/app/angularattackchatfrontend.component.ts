097734cba44540b49dbca6dc608651dc9a3a6b99
colan <cconnon11@gmail.com>
2016-05-13 20:27:08 -0400

    Added material css frameworks and made root component work

--- angularattackchatfrontend/src/app/angularattackchatfrontend.component.ts
+++ angularattackchatfrontend/src/app/angularattackchatfrontend.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 
+import {RootmessagecomponentComponent}  from './rootmessagecomponent/rootmessagecomponent.component';
+
 @Component({
   moduleId: module.id,
   selector: 'angularattackchatfrontend-app',
   templateUrl: 'angularattackchatfrontend.component.html',
-  styleUrls: ['angularattackchatfrontend.component.css']
+  styleUrls: ['angularattackchatfrontend.component.css'],
+  directives: [RootmessagecomponentComponent]
 })
 export class AngularattackchatfrontendAppComponent {
-  title = 'angularattackchatfrontend works!';
+
 }
