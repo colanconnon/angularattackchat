c6782084d307105c8c194bd47b3ed09603853910
colan <cconnon11@gmail.com>
2016-05-14 13:58:49 -0400

    Added routing

--- angularattackchatfrontend/src/app/angularattackchatfrontend.component.ts
+++ angularattackchatfrontend/src/app/angularattackchatfrontend.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-
+import {RouteConfig, ROUTER_DIRECTIVES} from '@angular/router-deprecated';
 import {RootmessagecomponentComponent}  from './rootmessagecomponent/rootmessagecomponent.component';
+import {LogincomponentComponent} from './logincomponent';
 
 @Component({
   moduleId: module.id,
   selector: 'angularattackchatfrontend-app',
   templateUrl: 'angularattackchatfrontend.component.html',
   styleUrls: ['angularattackchatfrontend.component.css'],
-  directives: [RootmessagecomponentComponent]
+  directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  {path: '/', component: RootmessagecomponentComponent, name:"Home", useAsDefault: true},
+  {path: '/login', component: LogincomponentComponent ,name:'Login'}
+])
 export class AngularattackchatfrontendAppComponent {
 
 }
--- angularattackchatfrontend/src/main.ts
+++ angularattackchatfrontend/src/main.ts
@@ -2,9 +2,10 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 import { AngularattackchatfrontendAppComponent, environment } from './app/';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AngularattackchatfrontendAppComponent, [HTTP_PROVIDERS]);
+bootstrap(AngularattackchatfrontendAppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS]);
