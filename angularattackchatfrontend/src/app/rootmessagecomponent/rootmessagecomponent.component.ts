f66f52eeeda3aa5ad8aa8c55bd75c1d87284a540
colan <cconnon11@gmail.com>
2016-05-13 21:51:17 -0400

    message box css working

--- angularattackchatfrontend/src/app/rootmessagecomponent/rootmessagecomponent.component.ts
+++ angularattackchatfrontend/src/app/rootmessagecomponent/rootmessagecomponent.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {ConversationlistcomponentComponent} from '../conversationlistcomponent/conversationlistcomponent.component';
+import { MessagelistcomponentComponent } from '../messagelistcomponent/';
+
 @Component({
   moduleId: module.id,
   selector: 'app-rootmessagecomponent',
   templateUrl: 'rootmessagecomponent.component.html',
   styleUrls: ['rootmessagecomponent.component.css'],
-  directives: [ConversationlistcomponentComponent]
+  directives: [ConversationlistcomponentComponent, MessagelistcomponentComponent]
 })
 export class RootmessagecomponentComponent implements OnInit {
 
