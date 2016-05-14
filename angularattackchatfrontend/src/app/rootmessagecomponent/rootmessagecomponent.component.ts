2339fcb226328b0ba8ff303419fef0c76e466aca
colan <cconnon11@gmail.com>
2016-05-13 20:44:27 -0400

    Got the conversation list component to show up

--- angularattackchatfrontend/src/app/rootmessagecomponent/rootmessagecomponent.component.ts
+++ angularattackchatfrontend/src/app/rootmessagecomponent/rootmessagecomponent.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ConversationlistcomponentComponent} from '../conversationlistcomponent/conversationlistcomponent.component';
 @Component({
   moduleId: module.id,
   selector: 'app-rootmessagecomponent',
   templateUrl: 'rootmessagecomponent.component.html',
-  styleUrls: ['rootmessagecomponent.component.css']
+  styleUrls: ['rootmessagecomponent.component.css'],
+  directives: [ConversationlistcomponentComponent]
 })
 export class RootmessagecomponentComponent implements OnInit {
 
