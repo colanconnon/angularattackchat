a252df5cd8fd7594d28218323bb9f804329d2277
colan <cconnon11@gmail.com>
2016-05-13 22:35:55 -0400

    Added some message form css

--- angularattackchatfrontend/src/app/rootmessagecomponent/rootmessagecomponent.component.ts
+++ angularattackchatfrontend/src/app/rootmessagecomponent/rootmessagecomponent.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {ConversationlistcomponentComponent} from '../conversationlistcomponent/conversationlistcomponent.component';
 import { MessagelistcomponentComponent } from '../messagelistcomponent/';
-
+import {MessageformcomponentComponent} from '../messageformcomponent';
 @Component({
   moduleId: module.id,
   selector: 'app-rootmessagecomponent',
   templateUrl: 'rootmessagecomponent.component.html',
   styleUrls: ['rootmessagecomponent.component.css'],
-  directives: [ConversationlistcomponentComponent, MessagelistcomponentComponent]
+  directives: [ConversationlistcomponentComponent, MessagelistcomponentComponent, MessageformcomponentComponent]
 })
 export class RootmessagecomponentComponent implements OnInit {
 
